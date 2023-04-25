7d85ec5c66cf3177918ff140325cd9f793c2cfa9
Justin Signo <justinsigno@yahoo.com>
2023-04-25 15:35:35 -0400

    done with variables

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+# variable
 print("hello world")
+
+name = "Justin"
+age = "25"
+print(name)
+print(age)
+# cannot name variables with numbers ex: 3name
+
+full_name = "justin signo"
+print(full_name)
+# this is call snake casing. used for longer variables 
+
+width, heigth = 400, 500
+print(width)
+print(heigth)
