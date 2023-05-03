c9ca552047e917c53000fcf03e8941db9b4230aa
Justin Signo <justinsigno@yahoo.com>
2023-05-03 14:53:41 -0400

    functions

---
+++ functions.py
@@ -0,0 +1,26 @@
+# No arguments
+def say_my_name():
+    print("hello justin")
+    print("hello leta")
+    print("hello david")
+
+# say_my_name()
+
+# One argument 
+def say_my_name_again(name):
+    print(name)
+
+# say_my_name_again("hello again justin")
+
+# Multiple arguments
+def greeting(greet, name):
+    '''
+    greeting takes in 2 arguments (greet and name)
+    '''
+    print(greet, name)
+
+# positional arguments
+# greeting("aloha","justin")
+
+# named arguments
+# greeting(greet="hello", name="david")
