7eeaa1636bf41fb9adf3bf0d9156e9e18ca207fb
Justin Signo <justinsigno@yahoo.com>
2023-05-03 17:02:42 -0400

    done for the day

---
+++ lists.py
@@ -0,0 +1,11 @@
+# LISTS / ARRAYS
+
+fruits = ["apple", "orange", "berry"]
+print(fruits)
+fruits.append("pear")
+print(fruits)
+fruits.append("strawberry")
+print(fruits)
+
+# methods vs functions
+# append() is a method and print() is a function
