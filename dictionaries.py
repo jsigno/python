6f45c4461700eea62a848f736089cf150e4051a9
Justin Signo <justinsigno@yahoo.com>
2023-05-12 17:40:44 -0400

    sets

--- dictionaries.py
+++ dictionaries.py
@@ -1,10 +1,11 @@
 '''
 DICTIONARIES
 '''
+# Mutable means changable 
 
 # person = {
-#     "name": "justin",
-#     "shirt":"black",
+#     "name": "justin", 
+#     "shirt":"black", 
 #     "pants": "blue"
 #     }
 
@@ -15,16 +16,36 @@ DICTIONARIES
 
 def introducer():
     person = {
-        "name": "justin",
-        "shirt": "black",
-        "pants": "blue",
-        "assets": 100,
-        "debt": 50,
-        "netWorth": lambda: person["assets"] - person["debt"],
-        "favFruits": ["oranges", "apples", "bananas"]
+    "name": "justin", 
+    "shirt":"black", 
+    "pants": "blue",
+    "assets": 100,
+    "debt": 50,
+    "netWorth": lambda: person["assets"] - person["debt"],
+    "favFruits": ["oranges", "apples", "bananas"]
     }
 
+    person["shirt"] = "orange"
+    person["assets"] = 1000
+
     print(f'hello my name is {person["name"]}, i have a {person["shirt"]} shirt, and wearing {person["pants"]} pants, and new worth is {person["netWorth"]()}, also my favorite fruits are {person["favFruits"]}')
+    print(person.values())
+    print(person.keys())
 
+    print(list(person.values()))
 
-introducer()
+# introducer()
+
+# dictionaries are ordered 
+
+'''
+Tuples ()
+'''
+# immutable - cannot change/update
+
+numbers = (1,2)
+print(numbers)
+print(type(numbers))
+print(numbers[0])
+
+ 
---
+++ sets.py
@@ -0,0 +1,21 @@
+'''
+SETS
+'''
+
+# {} used for getting unique elements
+
+words_1 = ["peanut", "sky", "flower", "fire"]
+words_2 = ["apple", "glass", "watch", "sky"]
+
+words = words_1 + words_2
+
+# combines the lists
+print(words) 
+
+# removes duplicates
+print(set(words))
+
+# SPEACIAL WORDS
+'''
+list, len, max, min, set, dict
+'''
